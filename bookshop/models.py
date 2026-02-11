3347a5414b058e3b707010d796c77b90855842ae
vol <ke2442cvv43@susu.ru>
2026-02-11 22:54:35 +0500

    p3 final version v2

--- bookshop/models.py
+++ bookshop/models.py
@@ -10,18 +10,3 @@ class Book(models.Model):
 
     def __str__(self):
         return f"{self.title} - {self.author} (Осталось: {self.stock})"
-
-class Profile(models.Model):
-    class Role(models.TextChoices):
-        USER = "user", "Обычный пользователь"
-        ADMIN = "admin", "Администратор"
-
-    user = models.OneToOneField(
-        settings.AUTH_USER_MODEL,
-        on_delete= models.CASCADE,
-        related_name="profile"
-    )
-    role = models.CharField(max_length=10, choices=Role.choices, default=Role.USER)
-
-    def __str__(self):
-        return f"{self.user.username} ({self.role})"
